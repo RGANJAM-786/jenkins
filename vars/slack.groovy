41188cdfcdcff1ee4d917d4d61ee99cf85be7725
RGANJAM-786 <rakesh173r1a0236@gmail.com>
2025-07-18 18:43:00 +0530

    Update slack.groovy

--- vars/slack.groovy
+++ vars/slack.groovy
@@ -1,36 +1,27 @@
 def call(String buildStatus = 'STARTED') {
-    // Default to SUCCESS if status is null or empty
-    buildStatus = buildStatus ?: 'SUCCESS'
+  // build status of null means successful
+  //This is the condition which we are checking weather buildStatus is SUCCESSFULL or not.
+ //This line updated to show the Eclipse with GitHub demo
+  buildStatus =  buildStatus ?: 'SUCCESS'
 
-    // Default values (assume failure)
-    def colorName = 'RED'
-    def colorCode = '#FF0000'
-    def subject = "${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'"
-    def summary = "${subject} (${env.BUILD_URL})"
+  // Default values
+  def colorName = 'RED'
+  def colorCode = '#FF0000'
+  def subject = "${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'"
+  def summary = "${subject} (${env.BUILD_URL})"
 
-    // Set color based on build status
-    switch(buildStatus) {
-        case 'STARTED':
-            colorName = 'YELLOW'
-            colorCode = '#FFFF00'
-            break
-        case 'SUCCESS':
-            colorName = 'GREEN'
-            colorCode = '#00FF00'
-            break
-        case 'FAILURE':
-            colorName = 'RED'
-            colorCode = '#FF0000'
-            break
-        default:
-            colorName = 'GRAY'
-            colorCode = '#AAAAAA'
-            summary = "UNKNOWN STATUS: ${summary}"
-    }
+  // Override default values based on build status
+  if (buildStatus == 'STARTED') {
+    colorName = 'YELLOW'
+    colorCode = '#FFFF00'
+  } else if (buildStatus == 'SUCCESS') {
+    colorName = 'GREEN'
+    colorCode = '#00FF00'
+  } else {
+    colorName = 'RED'
+    colorCode = '#FF0000'
+  }
 
-    // Send Slack notification
-    slackSend(color: colorCode, message: summary)
-
-    // Optionally log to Jenkins console
-    echo "Slack notification sent: ${summary} with color ${colorName}"
+  // Calling the slackSend function to Send notifications.
+  slackSend (color: colorCode, message: summary)
 }
